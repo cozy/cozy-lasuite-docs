2b6aeb45d9c02dc57082062808941027de88b9f2
Théo Poizat <hello@zatteo.com>
2025-06-03 22:44:45 +0200

    feat: Path in top bar

--- src/components/AppProviders.jsx
+++ src/components/AppProviders.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { BarProvider } from 'cozy-bar'
 import { CozyProvider } from 'cozy-client'
+import { RealTimeQueries } from 'cozy-client'
 import { DataProxyProvider } from 'cozy-dataproxy-lib'
 import { WebviewIntentProvider } from 'cozy-intent'
 import { BreakpointsProvider } from 'cozy-ui/transpiled/react/providers/Breakpoints'
@@ -11,6 +12,7 @@ const AppProviders = ({ client, lang, polyglot, children }) => {
   return (
     <WebviewIntentProvider>
       <CozyProvider client={client}>
+        <RealTimeQueries doctype="io.cozy.files" />
         <DataProxyProvider>
           <BarProvider>
             <I18n lang={lang} polyglot={polyglot}>
