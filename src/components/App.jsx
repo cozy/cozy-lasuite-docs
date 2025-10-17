17acf3830d01f5c22bf0295a00490fafe6945f58
Vince Linise <contact@vincelinise.com>
2025-10-17 15:13:29 +0200

    feat: Communication with both Iframe contexts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,11 @@ import styles from '../styles/iframes.styl'
 
 import { useLocation } from 'react-router-dom'
 import { Q, useClient, useQuery } from 'cozy-client'
+import { BarRight } from 'cozy-bar'
+
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
 
 const App = () => {
   const { pathname } = useLocation()
@@ -33,6 +38,9 @@ const App = () => {
   const [controllerAppUrl, setControllerAppUrl] = useState("http://drive.claude2.192-168-1-28.nip.io:8080/")
   const [controllerAppParams, setControllerAppParams] = useState(["runsInShell=true"])
 
+  const controllerApp = React.useRef(null)
+  const embeddedApp = React.useRef(null)
+
   const addOrReplaceParam = (key, value) => {
     const index = controllerAppParams.findIndex(param => param.startsWith(key + "="))
     if (index !== -1) {
@@ -56,12 +64,43 @@ const App = () => {
     addOrReplaceParam("selectedFile", currentlyOpenedFile.id)
   }, [currentlyOpenedFile])
 
+  useEffect(() => {
+    window.onmessage = function (e) {
+      if (e.data == undefined || e.data == null || typeof e.data !== "string") return;
+      if (e.data.startsWith("openFile:")) {
+        const fileId = e.data.split("openFile:")[1].trim();
+        console.log("should open ", fileId)
+        // redirect to same url but replace everything after docs/* by docs/{fileId}
+        const newURL = embeddedDocsUrl + "/docs/" + fileId;
+        console.log("newURL", newURL)
+        // embeddedApp.current.src = newURL;
+        embeddedApp.current.contentWindow.postMessage('openFile:' + fileId, '*');
+      }
+    };
+  }, [])
+
+  const createNewDocument = () => {
+    if (!embeddedApp.current) return;
+    embeddedApp.current.contentWindow.postMessage('newDoc', '*');
+  }
+
   return (
     <div className={styles["iframesContainer"]}>
-      <iframe className={styles["controllerApp"]} id="controllerApp" src={
+      <BarRight>
+        <Button
+          label={"Nouveau document"}
+          variant={"primary"}
+          size="small"
+          startIcon={<Icon icon={PlusIcon} />}
+          onClick={() => createNewDocument()}
+          className={"u-mr-1"}
+        />
+      </BarRight>
+
+      <iframe ref={controllerApp} className={styles["controllerApp"]} id="controllerApp" src={
         controllerAppUrl + (controllerAppParams.length > 0 ? "?" + controllerAppParams.join("&") : "")
       }></iframe>
-      <iframe className={styles["embeddedApp"]} id="embeddedApp" src={isReady ? urlToLoad : null}></iframe>
+      <iframe ref={embeddedApp} className={styles["embeddedApp"]} id="embeddedApp" src={isReady ? urlToLoad : null}></iframe>
     </div>
   )
 }
